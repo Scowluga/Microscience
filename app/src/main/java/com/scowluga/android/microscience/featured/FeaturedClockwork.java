68ffb448a3808d81a413fb2a406c6416f20b5c2c
David Lu <david.scowluga@gmail.com>
2017-05-29 23:31:01 -0400

    Button updates

--- app/src/main/java/com/scowluga/android/microscience/featured/FeaturedClockwork.java
+++ app/src/main/java/com/scowluga/android/microscience/featured/FeaturedClockwork.java
@@ -1,11 +1,14 @@
 package com.scowluga.android.microscience.featured;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.scowluga.android.microscience.R;
 
@@ -34,6 +37,16 @@ public class FeaturedClockwork extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment4_featured_clockwork, container, false);
+
+
+        Button launch = (Button)v.findViewById(R.id.kurzweil_launch);
+        launch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://microscience.on.ca/clockwork-2/"));
+                v.getContext().startActivity(intent);
+            }
+        });
 
         return v;
     }
--- app/src/main/java/com/scowluga/android/microscience/featured/FeaturedKurzweil.java
+++ app/src/main/java/com/scowluga/android/microscience/featured/FeaturedKurzweil.java
@@ -1,11 +1,14 @@
 package com.scowluga.android.microscience.featured;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.scowluga.android.microscience.R;
 
@@ -35,6 +38,15 @@ public class FeaturedKurzweil extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment4_featured_kurzweil, container, false);
 
+
+        Button launch = (Button)v.findViewById(R.id.kurzweil_launch);
+        launch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://microscience.on.ca/kurzweil-3000-web-firefly/"));
+                v.getContext().startActivity(intent);
+            }
+        });
         return v;
     }
 
